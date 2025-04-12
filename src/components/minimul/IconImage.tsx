ff3320dea706995e501e82b1862d85a5d67931b7
hosainahmed <hosainahmed534745@gmail.com>
2025-04-12 17:06:24 +0600

    update logo

--- src/components/minimul/IconImage.tsx
+++ src/components/minimul/IconImage.tsx
@@ -6,7 +6,7 @@ function IconImage() {
     <div>
       <div className="h-fit w-48 md:w-56 lg:w-64 xl:w-72">
         <Image
-          src="/logo SVG.svg"
+          src="/Frame-3.svg"
           alt="Logo"
           width={400}
           height={400}
