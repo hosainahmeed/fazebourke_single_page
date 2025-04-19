e4e1f4646baecfe3cc13a565bd63abe03c6f0be5
hosain ali <hosainahmed534745@gmail.com>
2025-04-19 07:36:45 +0600

    brand logo update

--- src/components/minimul/IconImage.tsx
+++ src/components/minimul/IconImage.tsx
@@ -6,7 +6,7 @@ function IconImage() {
     <div>
       <div className="h-fit w-48 md:w-56 lg:w-64 xl:w-72">
         <Image
-          src="/Bet_Collective (1) (1).svg"
+          src="/logo.png"
           alt="Logo"
           width={400}
           height={400}
